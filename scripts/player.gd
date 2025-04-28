30e9ee521538a0d6885678f55d7f804e753e6879
Thomas Miller <thomas.miller@epita.fr>
2025-04-28 21:32:50 +0200

    feat: sprite rotation

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,4 @@
-extends AnimatedSprite2D
+extends Node2D
 class_name Player
 
 const TILESIZE = 32
@@ -17,17 +17,30 @@ var target_position : Vector2
 func _ready() -> void:
 	state = playerState.IDLE
 
+func _process(delta: float) -> void:
+	if state == playerState.MOVING:
+		$AnimatedSprite2D.play("moving")
+	else:
+		$AnimatedSprite2D.play("idle")
+
 func _physics_process(delta: float) -> void:
 	if state == playerState.IDLE and other and other.state == playerState.IDLE or state == playerState.CAN_MOVE:
 		var direction : Vector2
 		if Input.is_action_pressed("up"):
 			direction = Vector2.UP
+			$AnimatedSprite2D.rotation = deg_to_rad(0)
 		elif Input.is_action_pressed("down"):
 			direction = Vector2.DOWN
+			$AnimatedSprite2D.rotation = deg_to_rad(180)
 		elif Input.is_action_pressed("left"):
 			direction = Vector2.LEFT
+			$AnimatedSprite2D.rotation = deg_to_rad(-90)
 		elif Input.is_action_pressed("right"):
 			direction = Vector2.RIGHT
+			$AnimatedSprite2D.rotation = deg_to_rad(90)
+		else:
+			state = playerState.IDLE
+			return
 		target_position = self.global_position + direction * TILESIZE
 		$RayCast2D.target_position = direction * TILESIZE
 		$RayCast2D.force_raycast_update()
@@ -36,6 +49,6 @@ func _physics_process(delta: float) -> void:
 			if other and other.state != playerState.MOVING:
 				other.state = playerState.CAN_MOVE
 	elif state == playerState.MOVING:
-		self.global_position = self.global_position.move_toward(target_position, 0.5)
+		self.global_position = self.global_position.move_toward(target_position, 2)
 		if self.global_position == target_position:
 			state = playerState.IDLE
