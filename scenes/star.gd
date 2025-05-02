8e41540f50b0b837adcc021f7cf52fa3056bba68
Harrys Kedjnane <harrys.kedjnane@epita.fr>
2025-05-03 01:12:43 +0200

    stars

---
+++ scenes/star.gd
@@ -0,0 +1,40 @@
+class_name Star extends TextureRect
+
+@export var from_center = true
+@export var time = 0.1
+@export var transition_type = Tween.TransitionType.TRANS_SPRING
+@export var scale_up_to = 1.5
+
+@export var button: bool = false : set = animate_star
+
+var default_transform : Transform2D
+
+@export var ON_STAR_TEXTURE : AtlasTexture
+@export var OFF_STAR_TEXTURE : AtlasTexture
+
+
+func animate_star(_a : bool) -> void:
+# Changes the star texture from black to gold and
+# adds an animation to scale up the star with a bounce using Tween.
+	if not is_inside_tree():
+		return
+	if _a == false:
+		self.texture = OFF_STAR_TEXTURE
+		self.scale = default_transform.get_scale()
+		return
+	self.texture = ON_STAR_TEXTURE
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "scale", Vector2(scale_up_to, scale_up_to), time).set_trans(transition_type)
+	#tween.tween_property($TextureRect, "modulate", Color(1, 1, 0), time).set_trans(Tween.TRANS_BOUNCE)
+	
+	
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	default_transform = self.get_transform()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
